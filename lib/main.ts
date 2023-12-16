99dae73a9d5ddedd11f7b976af10ef7f5008ed0a
Gino Rey <gino.rey@outlook.com>
2023-12-15 22:05:42 -0500

    fixed commit issue

---
+++ lib/main.ts
@@ -0,0 +1,30 @@
+import * as cdk from 'aws-cdk-lib';
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { Role, ServicePrincipal, PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { Construct } from 'constructs';
+import { lambdaConfigs, LambdaConfig } from '../config/lambda-configs';
+import { ApiGateway } from './api_gateway';
+
+export class API extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const api = new ApiGateway(this)
+
+    for (const config of lambdaConfigs) {
+    /**
+     * For each LambdaConfig, create a DockerImageFunction and add it to the API Gateway.
+     * - The DockerImageFunction is created from an image asset in the lambda-functions directory.
+     * - The function is named after the LambdaConfig, with spaces removed and all letters lowercased.
+     * - The function is added to the API Gateway resource with the path specified in the LambdaConfig.
+     */
+      const lambdaFunction = new lambda.DockerImageFunction(this, config.name, {
+        code: lambda.DockerImageCode.fromImageAsset(config.path),
+        timeout: cdk.Duration.seconds(10),
+        architecture: lambda.Architecture.ARM_64,
+      });
+      // @ts-ignore
+      api.addIntegration(config.corsConfig.allowMethods[0], config.url, lambdaFunction);
+    }
+  };
+}
